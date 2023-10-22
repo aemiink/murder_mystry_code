54fbe4456e3edd299cba3081d90dafb19e917ffc
aekkaya <145156509+aekkaya@users.noreply.github.com>
2023-10-22 13:37:58 +0300

    kodların açıklaması ile local script
    bir önceki commit'te açıklamasını yapmamıştık. Bu commit'te türkçe açıklamasınıda yapmak istedim.

--- localscript.lua
+++ localscript.lua
@@ -1,24 +1,42 @@
-local kopyaDeposu = game:GetService("ReplicatedStorage")
-local durumDegeri = kopyaDeposu:WaitForChild("DurumDegeri")
-local zamanDegeri = kopyaDeposu:WaitForChild("ZamanDegeri")
-local ekranArayuzu = script.Parent
-local durumBasligi = ekranArayuzu:WaitForChild("DurumBasligi")
+--> ReplicatedStorage'i de bir değişken içerisinde depolamam lazım
+local repStorage = game:GetService("ReplicatedStorage")
+--> ZamanDegerini bir değişken içerisinde depolamam lazım
+local zamanDegeri = repStorage:WaitForChild("ZamanDegeri")
+--> DurumDegerini de bir değişken içerisinde depolamam lazım.
+local durumDegeri = repStorage:WaitForChild("DurumDegeri")
+-->GUI
+local GUI = script.Parent
+--> GUI'nin içerisinde yer Text Label
+local durumTablosu = GUI:WaitForChild("DurumTablosu")
 
 
-function saniyedenDakikayaDonusturucu(saniyeler)
-	local dakikalar = math.floor(saniyeler / 60)
-	local kalanSaniyeler = saniyeler % 60
+--> Fonksiyon saniyeleri dakikaya çevirecek bir fonksiyon.
+function saniyedenDakikayaCeviri(saniye)
+	--> saniyeleri 60'a bölersek sonuç bize dakikayı verir.
+	local dakikalar = saniye / 60
+	--> Kalan saniyeyi bulabilmek için tüm saniyeleri 60'a böler kalanı alırız.
+	--> % işareti belirlediğimiz sayıyı 60'a böler ve kalanı alır.
+	--> math.floor() fonksiyonu ise matematiksel işlemleri yaparken aynı anda birden fazla işlem yaptığımızı belirler.
+	local kalanSaniyeler = math.floor(saniye % 60)
+	--> Her seferinde bu değerler değişeceği için bu fonksiyonu döndürmeliyiz.
+	--> return: anahtar kelimesi bir fonksiyonu dödnür.
+	--> tostring() fonksiyonu int olan bir değeri yazısal bir değere dönüştürür 
 	return tostring(dakikalar) .. ":" .. kalanSaniyeler
 end
 
-function durumBasligiGuncellemesi()
-	if zamanDegeri.Value > 0 then
-		durumBasligi.Text = durumDegeri.Value
+--> Durum Tablosunu güncelleyebilmek için yeni bir fonksiyon oluşturdum.
+function durumTablosunuGuncelleme()
+	-->Eğer benim ZamanDegeri'm 0'dan küçükse
+	if zamanDegeri.Value < 0 then
+		--> durumTablosu'nun yazısını durum Değerine eşitle.
+		durumTablosu.Text = durumDegeri.Value
+	--> Değilse
 	else
-		durumBasligi.Text = durumDegeri.Value .. "(" .. saniyedenDakikayaDonusturucu(zamanDegeri.Value) .. ")"
+		--> durumTablosunun yazısını durum değeri + saniyedendakakayaceviren fonksiyonun sayısına eşitle.
+		durumTablosu.Text = durumDegeri.Value .. "(" .. saniyedenDakikayaCeviri(zamanDegeri.Value) .. ")"
 	end
 end
 
-durumDegeri.Changed:Connect(durumBasligiGuncellemesi)
-zamanDegeri.Changed:Connect(durumBasligiGuncellemesi)
-durumBasligiGuncellemesi()
+zamanDegeri.Changed:Connect(durumTablosunuGuncelleme)
+durumDegeri.Changed:Connect(durumTablosunuGuncelleme)
+durumTablosunuGuncelleme()
